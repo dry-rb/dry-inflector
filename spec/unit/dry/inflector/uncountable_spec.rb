86327179c1b463de2680281cb125628042f89e46
Luca Guidi <me@lucaguidi.com>
2017-10-05 15:16:53 +0200

    Specs for Dry::Inflector#uncountable?

---
+++ spec/unit/dry/inflector/uncountable_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe Dry::Inflector do
+  describe "#uncountable?" do
+    subject { described_class.new }
+
+    it "returns true when empty string" do
+      expect(subject.uncountable?(i(""))).to be(true)
+    end
+
+    it "returns true when word is present in list" do
+      expect(subject.uncountable?(i("grass"))).to be(true)
+    end
+
+    it "returns false when word is not present in list" do
+      expect(subject.uncountable?(i("user"))).to be(false)
+    end
+
+    it "returns true when word is present in list but in different case" do
+      expect(subject.uncountable?(i("FiSH"))).to be(true)
+    end
+  end
+end
