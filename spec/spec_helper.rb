1571f2249a70d6e37a6b86bd5fd3e6a4ce3c5d0f
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-07 22:02:24 +0300

    Simplify coverage setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,17 +3,10 @@ require 'pathname'
 
 SPEC_ROOT = Pathname(__FILE__).dirname
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.load(File.read(SPEC_ROOT.join('../.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION >= latest_mri
-    require 'simplecov'
-
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
   end
 end
 
