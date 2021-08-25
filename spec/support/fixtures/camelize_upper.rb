d6c8fa3a2487b9045c05083cacdb6434f61ab904
Postmodern <postmodern.mod3@gmail.com>
2021-08-24 17:28:48 -0700

    Omit dashes when converting underscored/dashed strings to CamelCase.

--- spec/support/fixtures/camelize_upper.rb
+++ spec/support/fixtures/camelize_upper.rb
@@ -13,6 +13,7 @@ module Fixtures
     CASES = {
       "merb" => "Merb",
       "data_mapper" => "DataMapper",
+      "data-mapper" => "DataMapper",
       "dry/inflector" => "Dry::Inflector",
       "dry/inflector/inflections" => "Dry::Inflector::Inflections",
       "blog_post/author" => "BlogPost::Author",
