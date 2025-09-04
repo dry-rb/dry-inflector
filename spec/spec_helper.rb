6a2c9ef88922b5a0f401f2508863335b08cc8173
Adam Lassek <adam@doubleprime.net>
2025-09-03 23:41:05 -0500

    Migrate project to Hanakai repo-sync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,13 @@ require_relative "support/coverage"
 
 require "pathname"
 
-SPEC_ROOT = Pathname(__FILE__).dirname
+SPEC_ROOT = Pathname(__dir__)
+
+require "dry/inflector"
+
+Dir.glob(Pathname.new(__dir__).join("support", "**", "*.rb")).each do |file|
+  require_relative file
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -16,23 +22,8 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  config.filter_run_when_matching :focus
-  config.disable_monkey_patching!
-
-  config.warnings = true
-
   config.default_formatter = "doc" if config.files_to_run.one?
   config.profile_examples = 10
-
   config.order = :random
   Kernel.srand config.seed
-end
-
-require "dry/inflector"
-
-Dir.glob(Pathname.new(__dir__).join("support", "**", "*.rb")).each do |file|
-  require_relative file
 end
