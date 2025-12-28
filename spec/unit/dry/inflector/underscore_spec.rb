846ddcc7d576b173b49c5b6102b687620eae3fe5
Tim Riley <tim@riley.id.au>
2025-12-28 11:39:37 +1100

    Fix rubocop issue

--- spec/unit/dry/inflector/underscore_spec.rb
+++ spec/unit/dry/inflector/underscore_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Dry::Inflector do
       expect(subject.underscore(i("OpenSSL::HMAC"))).to eql("openssl/hmac")
       expect(subject.underscore(i("OpenSSL::Digest"))).to eql("openssl/digest")
     end
-    
+
     it "handles plural acronyms" do
       expect(subject.underscore(i("CustomerAPIs"))).to eql("customer_apis")
     end
